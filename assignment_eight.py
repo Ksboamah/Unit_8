0582efac4ed1cb358643b06505ca14d3eff9680e
Kwame <bkwamesakyi@gmail.com>
2019-11-19 12:07:25 -0500

    I made a basic graphic for a DandD dice roller. I started my Unit 8 daily exercises.

---
+++ assignment_eight.py
@@ -0,0 +1,14 @@
+import tkinter
+
+root = tkinter.Tk()
+operation = tkinter.IntVar()
+
+
+
+title_label = tkinter.Label(root, text="Calculator")
+title_label.grid(row=1, column=1)
+
+
+
+
+root.mainloop()
